34da04d3f4f96438306d130b7e9ad8f50e3b4353
Michal Borychowski <807297+boryn@users.noreply.github.com>
2021-05-18 16:52:20 +0200

    Minor change to PlanSubscriptionNotFound exception (#55)

--- src/Exceptions/PlanSubscriptionNotFound.php
+++ src/Exceptions/PlanSubscriptionNotFound.php
@@ -10,7 +10,7 @@ class PlanSubscriptionNotFound extends \InvalidArgumentException
 {
     public function __construct($subscriptionTag = "", $code = 0, Throwable $previous = null)
     {
-        $message = "Subscription {$subscriptionTag} not found in subscriber.";
+        $message = "Subscription '{$subscriptionTag}' not found in subscriber.";
 
         parent::__construct($message, $code, $previous);
     }
