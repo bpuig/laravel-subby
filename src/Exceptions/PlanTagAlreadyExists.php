09a981df45dc0b49f778a32d6ff3c1d408980529
Michal Borychowski <807297+boryn@users.noreply.github.com>
2021-05-18 16:52:58 +0200

    Minor change to PlanTagAlreadyExists exception (#54)

--- src/Exceptions/PlanTagAlreadyExists.php
+++ src/Exceptions/PlanTagAlreadyExists.php
@@ -10,7 +10,7 @@ class PlanTagAlreadyExists extends \InvalidArgumentException
 {
     public function __construct($planTag = "", $code = 0, Throwable $previous = null)
     {
-        $message = "Plan tag {$planTag} already exists.";
+        $message = "Plan tag '{$planTag}' already exists.";
 
         parent::__construct($message, $code, $previous);
     }
