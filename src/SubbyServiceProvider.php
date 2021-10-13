31ba164a0c2cd9775b166486779702aa4c9b5ae0
Michal Borychowski <807297+boryn@users.noreply.github.com>
2021-10-13 19:06:33 +0200

    Fixes for publishing of the v5 and v5 upgrade migrations (#104)
    * Fixes for publishing of the v5 and v5 upgrade migrations
    * Added strict types at v5 migrations

--- src/SubbyServiceProvider.php
+++ src/SubbyServiceProvider.php
@@ -56,13 +56,13 @@ class SubbyServiceProvider extends ServiceProvider
             __DIR__ . '/../database/migrations/create_plan_subscriptions_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 2) . '_create_plan_subscriptions_table.php'),
             __DIR__ . '/../database/migrations/create_plan_subscription_features_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 3) . '_create_plan_subscription_features_table.php'),
             __DIR__ . '/../database/migrations/create_plan_subscription_usage_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 4) . '_create_plan_subscription_usage_table.php'),
-            __DIR__ . '/../database/migrations/create_plan_subscription_schedules_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 5) . '_create_plan_subscription_usage_table.php')
+            __DIR__ . '/../database/migrations/create_plan_subscription_schedules_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 5) . '_create_plan_subscription_schedules_table.php')
         ], 'subby.migrations');
 
         $this->publishes([
             __DIR__ . '/../database/migrations/v5.0.0/alter_plans_table_v5.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_alter_plans_table_v5.php'),
             __DIR__ . '/../database/migrations/v5.0.0/alter_plan_subscriptions_table_v5.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 1) . '_alter_plan_subscriptions_table_v5.php'),
-            __DIR__ . '/../database/migrations/v5.0.0/create_plan_subscription_schedules_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 2) . '_alter_plan_subscriptions_table_v5.php'),
+            __DIR__ . '/../database/migrations/v5.0.0/create_plan_subscription_schedules_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time() + 2) . '_create_plan_subscription_schedules_table.php'),
         ], 'subby.migrations.v5.0.0');
     }
 
